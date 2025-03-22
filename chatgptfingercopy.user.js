a722d36e041d47fa50f2a4f70c6ed80f637283e3
xiniu2023 <licong1309401095@gmail.com>
2025-03-22 12:23:42 +0800

    update

--- chatgptfingercopy.user.js
+++ chatgptfingercopy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChatGPT Swipe to Input
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @updateURL    https://aimoment29.github.io/PublicTemp/chatgptfingercopy.user.js
 // @description  检测横向滑动并将文本填入ChatGPT输入框
 // @author       xiniu
